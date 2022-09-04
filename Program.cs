ac373f4431bdec0ac2b940e602fd089ec50cb26f
Hugo Garcia <hhugokta@hotmail.com>
2022-09-03 21:27:14 -0300

    declarando tipos de dados

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿using curso.Models;
 
+string cidade = "São Paulo";
+int quantidade = 10;
+double preco = 10.50;
+decimal valor = 10.50m;
+bool ativo = true;
+
+Console.WriteLine("A cidade é: " + cidade);
+Console.WriteLine("A quantidade é: " + quantidade);
+Console.WriteLine("O preço é: " + preco.ToString("0.0000"));
+Console.WriteLine("O valor é: " + valor);
+Console.WriteLine("O ativo é: " + ativo);
 
 Pessoa p1 = new Pessoa();
 
